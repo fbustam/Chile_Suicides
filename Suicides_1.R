4fcc0e8548152e6b7e9dbc6342318ac3647d4cec
fbustam <fbustam@gmail.com>
2024-04-26 09:31:37 -0400

    Intro

---
+++ Suicides_1.R
@@ -0,0 +1,7 @@
+##########################################
+# ESTUDIO SERIE DE TIEMPO.               #
+# MORTALIDAD POR SUICIDO EN CHILE.       #
+# 2000-2020.                             #
+# Francisco Bustamante                   #
+# 23-IV-2024                             #
+##########################################
