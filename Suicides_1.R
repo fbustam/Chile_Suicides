2e8f81cc7c03e3a895dfc9039da192614417d88c
fbustam <fbustam@gmail.com>
2024-04-27 09:40:54 -0400

    Depuración de base de datos

--- Suicides_1.R
+++ Suicides_1.R
@@ -5,3 +5,25 @@
 # Francisco Bustamante                   #
 # 23-IV-2024                             #
 ##########################################
+
+library(readxl)
+library(tidyverse)
+
+
+# Importar base de datos --------------------------------------------------
+
+# Base de datos de suicidios Chile 2000-2020 (DEIS)
+
+df2 <- read_excel("df2.xlsx")
+View(df2)
+
+# Cambiamos la variable 'fecha de defunción'  a "date", "numeric"
+
+df2 |> 
+  mutate(FECHA_DEF = as.Date(FECHA_DEF, format = "%Y-%m-%d")) -> df2
+
+# Cambiamos categoría de otras variables
+df2$EDAD_CANT <- as.numeric(df2$EDAD_CANT)
+df2$ANO_DEF <- as.numeric(df2$ANO_DEF)
+
+view(df2)
