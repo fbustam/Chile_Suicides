daefba6d884df25a64dfe0154c5e03f99493508d
fbustam <fbustam@gmail.com>
2024-04-27 09:41:35 -0400

    Actualización de base de datos

--- Suicides_1.R
+++ Suicides_1.R
@@ -13,17 +13,34 @@ library(tidyverse)
 # Importar base de datos --------------------------------------------------
 
 # Base de datos de suicidios Chile 2000-2020 (DEIS)
-
 df2 <- read_excel("df2.xlsx")
 View(df2)
 
 # Cambiamos la variable 'fecha de defunción'  a "date", "numeric"
-
 df2 |> 
   mutate(FECHA_DEF = as.Date(FECHA_DEF, format = "%Y-%m-%d")) -> df2
 
 # Cambiamos categoría de otras variables
 df2$EDAD_CANT <- as.numeric(df2$EDAD_CANT)
 df2$ANO_DEF <- as.numeric(df2$ANO_DEF)
+df2$CODIGO_COMUNA_RESIDENCIA <- as.character(df2$CODIGO_COMUNA_RESIDENCIA)
 
-view(df2)
+# Acortamos la tabla y sacamos los NA
+df2 |> 
+  select(ANO_DEF, FECHA_DEF, GLOSA_SEXO, EDAD_CANT, GLOSA_COMUNA_RESIDENCIA, GLOSA_REG_RESIDENCIA, CODIGO_COMUNA_RESIDENCIA) |> 
+  drop_na() -> df2
+
+# Rename, ordenar y crear nueva columna con número identificador
+df2 |> 
+  rename(ano_def = ANO_DEF,
+         fecha_def = FECHA_DEF,
+         sexo =  GLOSA_SEXO,
+         edad = EDAD_CANT,
+         comuna = GLOSA_COMUNA_RESIDENCIA,
+         region = GLOSA_REG_RESIDENCIA,
+         codigo_comuna = CODIGO_COMUNA_RESIDENCIA)  |> 
+  arrange(fecha_def) |> 
+  mutate(ID = 1:38084) |> 
+  select(ID, everything()) -> df2
+View(df2)
+
